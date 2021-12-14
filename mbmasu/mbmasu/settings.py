a5338e92ef5af1093183215dcf5c27f7581ceb7e
Alex <bomvendador@yandex.ru>
2021-12-14 16:29:31 +0300

    namespace

--- mbmasu/mbmasu/settings.py
+++ mbmasu/mbmasu/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@)&9$dvz&&dqo4e2!v*kn6eaazz7%+a)8rm&)dse*fj$ys3q4-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['10.0.114.230', 'localhost', '127.0.0.1']
 
@@ -73,28 +73,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mbmasu.wsgi.application'
 
-if DEBUG:
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'mbm_db',
-            'USER': 'postgres',
-            'PASSWORD': 'gjkI87',
-            'HOST': 'localhost',
-            'PORT': '',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mbm_db',
+        'USER': 'postgres',
+        'PASSWORD': 'gjkI87',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
