5e93797cf279c049066b3169ceb269a30606e3ae
Alex <bomvendador@yandex.ru>
2021-12-15 16:43:25 +0300

    namespace

--- mbmasu/dash/views.py
+++ mbmasu/dash/views.py
@@ -1048,7 +1048,7 @@ def save_ready_for_ok_orders(request):
         data = request.POST
         next_ok_arr = data.getlist('next_ok_arr')
         passed_ok_arr = data.getlist('passed_ok_arr')
-        if data.get('ok_date')
+        if data.get('ok_date'):
             ok_date = date_to_db(data['ok_date'])
         commission_id = data.get('commissionID')
 
